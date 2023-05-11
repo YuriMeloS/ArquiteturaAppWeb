40e0ecfd29f156a9fdc1b0967320d763ef5cb5a8
YuriMeloS <65675262+YuriMeloS@users.noreply.github.com>
2023-05-10 21:19:20 -0300

    Add files via upload

--- oauth2-node-server/index.js
+++ oauth2-node-server/index.js
@@ -1,9 +1,8 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
 const express = require("express");
 const axios = require("axios");
 var cors = require("cors");
-const CLIENT_ID = "<dce22d6fa62972b24755>";
-const CLIENT_SECRET = "<7a63620d3e7cd4baf01357b3d17a892650de46dd>";
+const CLIENT_ID = "dce22d6fa62972b24755";
+const CLIENT_SECRET = "7a63620d3e7cd4baf01357b3d17a892650de46dd";
 const GITHUB_URL = "https://github.com/login/oauth/access_token";
 const app = express();
 app.use(cors({ credentials: true, origin: true }));
